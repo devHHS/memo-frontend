cdde1723b5aea0a94ca99d85365ff91163cdc384
devHHS <ham.hyosuck@gmail.com>
2025-09-09 20:25:11 +0200

    2. Tag Verbunden zwischen Backend und Frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,38 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient} from '@angular/common/http';
 import { Component } from '@angular/core';
+
+interface Memo {
+  id: number;
+  content: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'memo-frontend';
+  title = 'my-angular-app';
+
+  readonly API_URL = 'http://localhost:8080/memos';
+
+  memos: Memo[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getMemos();
+  }
+
+  getMemos() {
+    this.http.get<Memo[]>(this.API_URL)
+      .subscribe(memos => {
+        this.memos = memos;
+      });
+  }
 }
